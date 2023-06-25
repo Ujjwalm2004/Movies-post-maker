20ce83c83d7f3490eae36c168c062f122495241b
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-06-25 15:21:25 +0530

    Done

---
+++ movies_post_maker.py
@@ -0,0 +1,85 @@
+head = "<!DOCTYPE html PUBLIC \"-//WAPFORUM//DTD XHTML Mobile 1.0//EN\" \"http://www.wapforum.org/DTD/xhtml-mobile10.dtd\">\n<html>\n<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n<style type=\"text/css\">\nbody { background: ; \ncolor: ; \n } \n a { color: #960000; \n } \n a {text-decoration:none;}a:active{color: #058ADC;}\na:focus, a:hover { color: #ff1919;}\n</style>\n<link href=\"/css/style.css\" type=\"text/css\" rel=\"stylesheet\"/>\n\n<link rel=\"shortcut icon\" href=\"/images/favicon.ico\" type=\"image/x-icon\">\n<b><div><script type=\"text/javascript\"> document.title = \"M2links.cf ||M2links -Bengali HD Pc Movies Download Bollywood HD Pc Movies Download Hollywood Hindi Dudded Hollywood PC HD Movies Download Bengali 3gp MP4 Download 300 700 1080p HD PC movies</script></div></b>\n\n<meta name='viewport' content='width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=yes' />\n<meta name='revisit-after' content='1' />\n<meta name='language' content='EN' />\n<meta name=\"google-site-verification\" content=\"vI-Ig5OdQgMrvhaG7WQhr3ncR1yBhdidvzS-CSfMfAk\" />\n<meta name=\"google-site-verification\" content=\"jeqyjOCxWztOO971-ifQpBfcliIonINLeIBEeg858pY\" />\n</head>\n\n<body>\n\n<center><a href=\"https://telegram.me/M2links\"><div style=\"background:#1E90FF;font-weight:bold;color:#fff;display:inline-block;padding:8px 30px;margin:20px;border-radius:10px;\"><i class=\"fa fa-telegram\"></i><b>Join Us On Telegram </b></div></a></center>"
+
+center1 = "\n\n<center><a><div style=\"background:#DD0808;font-weight:bold;color:#fff;display:inline-block;padding:5px 20px;margin:1px;border-radius:10px;\"><b>"
+
+M_name = input("Enter Movie / Series Name : ")
+
+center2 = "</b></div></a></center> <div align='center' class='movielist'><img src='"
+
+M_URL = input("Enter Movie Image URL :")
+
+center3 = "' width='300' height='250'style=\"margin:20px;\"/></div>\n\n"
+
+#Movies Information
+
+M_info1 = "<center><a><div style=\"background:#DD0808;font-weight:bold;color:#fff;display:inline-block;padding:5px 20px;margin:1px;border-radius:10px;\"><b>Movie Information </b></div></a></center>\n\n<?-- Movie Information --?>\n\n<center>\n<div class='Let'><p style=\"color:white;\"</p><b>Genre : </b>"
+
+M_genre = input("Enter Movie Genre : ")
+
+M_info2 = "</div>\n<div class='Let'><p style=\"color:white;\"</p><b>Language : </b>"
+
+M_Lang = input("Enter Movie Lang : ")
+
+M_info3 = "</div>\n<div class='Let'><p style=\"color:white;\"</p><b>Quality : </b>"
+
+M_Qlity = input("Enter Movie Quality : ")
+
+M_info4 = "</div>\n<div class='Let'><p style=\"color:white;\"</p><b>Format : </b>MKV</div>\n<div class='Let'><p style=\"color:white;\"</p><b>Release Date : </b>"
+
+M_date = input("Enter Movie Release Date(EX. 16 December 2022) : ")
+
+M_info5 = "</div>\n<div class='Let'><p style=\"color:white;\"</p><b>Stars : </b>"
+
+M_Stars = input("Enter Movie Stars Names : ")
+
+M_info6 = "</div>\n<div class='Let'><p style=\"color:white;padding:8px 30px;margin:1px;\"</p><b>Story : </b>"
+
+M_story = input("Enter Movie Story : ")
+
+M_infoEnd = "</div></center>\n\n"
+
+#Screenshots 
+SS = "<!-- Movies Sreenshots -->\n\n"
+
+SS0 = "<center><div style=\"background:#DD0808;font-weight:bold;color:#fff;font-size:20px;display:inline-block;padding:6px 20px;margin:10px;border-radius:10px;\"><h6><b>ScreenShots</b></h6></div></center>\n\n<!-- Movies Sreenshots Enter Here -->\n\n"
+
+SS1 = "<div class='L'>\n<center><p style=\"color:white;padding:8px 30px;margin:1px;\"</p><img src='"
+URL1 = input("Enter Movie SS1 URL : ")
+
+SS2 = "'/></center>\n<center><p style=\"color:white;padding:8px 30px;margin:1px;\"</p><img src='"
+URL2 = input("Enter Movie SS2 URL : ")
+
+SS3 ="'/></center>\n<center><p style=\"color:white;padding:8px 30px;margin:1px;\"</p><img src='"
+URL3 = input("Enter Movie SS3 URL : ")
+
+SS4 = "'/></center>\n<center><p style=\"color:white;padding:8px 30px;margin:1px;\"</p><img src='"
+URL4 = input("Enter Movie SS4 URL : ")
+
+SS_END = "'/></center>\n</div>"
+
+DOWNBTN = "\n\n<center><div style=\"background:#DD0808;font-size:20px;font-weight:bold;color:#fff;display:inline-block;padding:6px 20px;margin:10px;border-radius:10px;\"><b>  Download Links  </b></div></center>"
+
+M_DOWN1 = "\n\n<!-- DOWNLOAD LINKS 480p 720p 1080p -- M2links.cf -->\n\n<center><a href=\""
+
+QULT1 = input("Enter Download Link 480p : ")
+QUL1 = "\"><div style=\"background:white;font-weight:bold;color:black;display:inline-block;padding:6px 30px;margin:5px;border-radius:10px;\"><b>480p ["
+size1 = input("Enter 480p Size :")
+
+M_DOWN2 = "]</b></div></a>\n<a href=\""
+QULT2 = input("Enter Download Link 720p HEVC : ")
+QUL2 = "\"><div style=\"background:white;font-weight:bold;color:black;display:inline-block;padding:6px 7px;margin:5px;border-radius:10px;\"><b>720p HEVC ["
+size2 = input("Enter 720p HEVC Size :")
+
+M_DOWN3 = "]</b></div></a></center>\n<center><a href=\""
+QULT3 = input("Enter Download Link 720p : ")
+QUL3 = "\"><div style=\"background:white;font-weight:bold;color:black;display:inline-block;padding:6px 35px;margin:4px;border-radius:10px;\"><b>720p ["
+size3 = input("Enter 720p Size :")
+
+M_DOWN4 = "]</b></div></a>\n<a href=\""
+QULT4 = input("Enter Download Link 1080p : ")
+QUL4 = "\"><div style=\"background:white;font-weight:bold;color:black;display:inline-block;padding:6px 35px;margin:4px;border-radius:10px;\"><b>1080p ["
+size4 = input("Enter 1080p Size :")
+
+END = "]</b></div></a></center>\n</body>\n</html>"\nCreated By @iSmartBoi_Ujjwal
+
+print(head,center1,M_name,center2,M_URL,center3,M_info1,M_genre,M_info2,M_Lang,M_info3,M_Qlity,M_info4,M_date,M_info5,M_Stars,M_info6,M_story,M_infoEnd,SS,SS0,SS1,URL1,SS2,URL2,SS3,URL3,SS4,URL4,SS_END,DOWNBTN,M_DOWN1,QULT1,QUL1,size1,M_DOWN2,QULT2,QUL2,size2,M_DOWN3,QULT3,QUL3,size3,M_DOWN4,QULT4,QUL4,size4,END)
